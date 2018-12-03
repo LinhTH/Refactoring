d18bd651747ea2514cbec6079ef7d7b7b4afc909
Linh Truong (extern) (U806345) <linh.truong@mobi.ch>
2018-12-03 13:32:07 +0700

    - First codes for Chain of responsibility

--- src/main/java/designpattern/behavioral/chainofresponsibility/Demo.java
+++ src/main/java/designpattern/behavioral/chainofresponsibility/Demo.java
@@ -1,4 +1,29 @@
 package designpattern.behavioral.chainofresponsibility;
 
+import designpattern.behavioral.chainofresponsibility.middleware.Middleware;
+import designpattern.behavioral.chainofresponsibility.middleware.RoleCheckMiddleWare;
+import designpattern.behavioral.chainofresponsibility.middleware.ThrottlingMiddleware;
+import designpattern.behavioral.chainofresponsibility.middleware.UserExistsMiddleWare;
+import designpattern.behavioral.chainofresponsibility.server.Server;
+
 public class Demo {
+    private static Server server;
+
+    private static void init() {
+        server = new Server();
+        server.register("admin@example.com", "admin_pass");
+        server.register("user@example.com", "user_pass");
+
+        Middleware middleware = new ThrottlingMiddleware(2);
+        middleware.linkWith(new UserExistsMiddleWare(server))
+                  .linkWith(new RoleCheckMiddleWare());
+
+        server.setMiddleware(middleware);
+    }
+
+    public static void main(String[] args)  {
+        init();
+
+        server.logIn("admin@example.com", "user_pass");
+    }
 }
